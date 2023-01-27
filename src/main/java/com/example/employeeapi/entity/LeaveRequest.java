ee1e039dfb65b33fbe6c91edfdce38267d23f152
Göksenin Gözde <goksenin.gozde@krontech.com>
2023-01-27 11:21:31 +0300

    fix: review

--- src/main/java/com/example/employeeapi/entity/LeaveRequest.java
+++ src/main/java/com/example/employeeapi/entity/LeaveRequest.java
@@ -16,7 +16,7 @@ import java.time.LocalDate;
 @Builder
 public class LeaveRequest {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
